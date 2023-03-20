123b4a3d7a04b4f705874d7f3027d106c202076c
LucaRondini <luca.rondini@mail.polimi.it>
2023-03-20 21:02:56 +0100

    Created GoalCard component

--- lib/components/cards.dart
+++ lib/components/cards.dart
@@ -16,7 +16,8 @@ class SessionCard extends StatelessWidget {
     return Card(
       child: InkWell(
         // This widget creates a feedback animation when the user taps on the card
-        onTap: () => print("Card tap"), // TODO: The callback should show details about the session
+        onTap: () => print("Card tap"),
+        // TODO: The callback should show details about the session
         child: Column(
           children: [
             const FractionallySizedBox(widthFactor: 1), // The box should take the entire width of the screen
@@ -32,21 +33,67 @@ class SessionCard extends StatelessWidget {
                     children: [
                       Icon(
                         Icons.timer,
-                        color: Theme.of(context).primaryColor,
+                        color: Theme
+                            .of(context)
+                            .primaryColor,
                       ),
-                      Text("${duration~/60} h ${duration%60} min"),
+                      Text("${duration ~/ 60} h ${duration % 60} min"),
                     ],
                   ),
                   Row(
                     children: [
                       Icon(
                         Icons.route,
-                        color: Theme.of(context).primaryColor,
+                        color: Theme
+                            .of(context)
+                            .primaryColor,
                       ),
                       Text("$distance km"),
                     ],
                   ),
                 ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GoalCard extends StatelessWidget {
+  String title;
+  bool isCompleted;
+  double targetValue;
+  double currentValue;
+  // TODO: Add callback or info about goal ID
+
+  GoalCard(this.title, this.isCompleted, this.targetValue, this.currentValue, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String status;
+    if(isCompleted) {
+      status = "Completed";
+    }
+    else {
+      status = "In progress";
+    }
+    return Card(
+      child: InkWell(
+        onTap: () => print("Card tap"),
+        child: Column(
+          children: [
+            const FractionallySizedBox(widthFactor: 1),
+            ListTile(
+              title: Text(title),
+              subtitle: Text(status),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: LinearProgressIndicator(
+                value: currentValue / targetValue,
+                backgroundColor: Theme.of(context).focusColor,
               ),
             ),
           ],
