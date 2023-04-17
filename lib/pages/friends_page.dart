5f073ca418be821277f88fda3ce70c30d502c7bf
LucaRondini <luca.rondini@mail.polimi.it>
2023-04-17 12:59:38 +0200

    Friend list tab in Friends Page is now dynamic. Small fixes to Trainings tab.

--- lib/pages/friends_page.dart
+++ lib/pages/friends_page.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import '../app_logic/auth.dart';
 import '../components/tiles.dart';
 import '../components/cards.dart';
+import '../model/user.dart';
 
 class FriendsPage extends StatelessWidget {
   const FriendsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var padding = MediaQuery.of(context).size.width / 40;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -26,14 +28,19 @@ class FriendsPage extends StatelessWidget {
           children: [
             ListView(
               children: [
+                Padding(padding: EdgeInsets.all(padding)),
                 FutureBuilder(
                     future: getProposals(),
                     builder: (context, snapshot) {
-                      // TODO: Add case to handle error snapshot.hasError
+                      if (snapshot.hasError) {
+                        return const Text(
+                          "Something went wrong. Please try again later.",
+                          textAlign: TextAlign.center,
+                        );
+                      }
                       if (snapshot.hasData) {
                         // This returns true even if there are no documents in the list
                         if (snapshot.data!.isEmpty) {
-                          // If there are no sessions, we print a message
                           return const Text(
                             "There are no proposals made by people that have added you to their friends.",
                             textAlign: TextAlign.center,
@@ -41,7 +48,6 @@ class FriendsPage extends StatelessWidget {
                         }
                         List<Widget> sessionList = [];
                         for (var proposal in snapshot.data!) {
-                          // For each session, we create a card and append it to the array of children
                           sessionList.add(TrainingProposalCard(proposal));
                         }
                         return Column(
@@ -58,22 +64,58 @@ class FriendsPage extends StatelessWidget {
             ),
             Center(
               child: ListView(
-                children: <Widget>[
-                  const Padding(padding: EdgeInsets.all(10)),
-                  Tile(
-                      icon: Icons.account_circle,
-                      title: "Luca Rondini",
-                      subtitle: "",
-                      callback: print),
-                  Tile(
-                      icon: Icons.account_circle,
-                      title: "Federico Gibellini",
-                      subtitle: "",
-                      callback: print),
-                  const Center(
-                      child: Text(
-                          "Use the Search section to find friends to add."))
+                children: [
+                  Padding(padding: EdgeInsets.all(padding)),
+                  FutureBuilder(
+                      future: getFriends(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return const Text(
+                            "Something went wrong. Please try again later.",
+                            textAlign: TextAlign.center,
+                          );
+                        }
+                        if (snapshot.hasData) {
+                          // This returns true even if there are no documents in the list
+                          if (snapshot.data!.isEmpty) {
+                            return const Text(
+                              "You haven't added any friends yet.",
+                              textAlign: TextAlign.center,
+                            );
+                          }
+                          List<Widget> friendList = [];
+                          for (var friend in snapshot.data!) {
+                            friendList.add(UserTile.fromUser(User(
+                                name: friend.get("name"),
+                                surname: friend.get("surname"))));
+                          }
+                          return Column(
+                            children: friendList,
+                          );
+                        } else {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      }),
+                  const Text(
+                    "Use the Search section to find friends to add.",
+                    textAlign: TextAlign.center,
+                  ),
                 ],
+                // children: <Widget>[
+                //   const Padding(padding: EdgeInsets.all(10)),
+                //   Tile(
+                //       icon: Icons.account_circle,
+                //       title: "Luca Rondini",
+                //       subtitle: "",
+                //       callback: print),
+                //   Tile(
+                //       icon: Icons.account_circle,
+                //       title: "Federico Gibellini",
+                //       subtitle: "",
+                //       callback: print),
+                // ],
               ),
             ),
           ],
@@ -88,20 +130,42 @@ class FriendsPage extends StatelessWidget {
         .doc(Auth().currentUser?.uid);
 
     // Get the list of people that have added the current user to their friends
-    final friendOfList = FirebaseFirestore.instance.collection("users").where("friends", arrayContains: userDocRef);
+    final friendOfList = FirebaseFirestore.instance
+        .collection("users")
+        .where("friends", arrayContains: userDocRef);
     final friendOfSnapshot = await friendOfList.get();
     final friendOfDocs = friendOfSnapshot.docs;
     List<DocumentReference> friendOfDocRefs = [];
-    for(var friendDoc in friendOfDocs) {
+    for (var friendDoc in friendOfDocs) {
       friendOfDocRefs.add(friendDoc.reference);
     }
 
+    if (friendOfDocRefs.isEmpty) {
+      // We need this check since whereIn doesn't accept an empty array
+      List<DocumentSnapshot> emptyList = [];
+      return emptyList;
+    }
     // Get the proposals made by all the people returned by the previous query that are not expired
     final docProposals = FirebaseFirestore.instance
         .collection("proposals")
-        .where("owner", whereIn: friendOfDocRefs)
+        .where("owner",
+            whereIn: friendOfDocRefs) // TODO: Split friendOfDocRefs if the length is > 10 because of whereIn limit
         .where("dateTime", isGreaterThanOrEqualTo: Timestamp.now());
     final querySnapshot = await docProposals.get(); // This get returns QuerySnapshot
     return querySnapshot.docs; // Return the list of DocumentSnapshot
+  }
+
+  Future<List<DocumentSnapshot>> getFriends() async {
+    final userDocRef = FirebaseFirestore.instance
+        .collection("users")
+        .doc(Auth().currentUser?.uid);
+
+    final userSnapshot = await userDocRef.get();
+    final friendRefs = userSnapshot.get("friends");
+    List<DocumentSnapshot> friendDocs = [];
+    for (var friend in friendRefs) {
+      friendDocs.add(await friend.get());
+    }
+    return friendDocs; // Return the list of DocumentSnapshot
   }
 }
