37038f571b68eaac8374afb44f52f20ca936eb19
Federico Gibellini <federico.gibellini@mail.polimi.it>
2023-06-16 08:34:08 +0200

    Removed WidgetTree
    Moved content of WidgetTree inside main.dart. Created class CustomApp, might be useful in testing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:progetto/app_logic/widget_tree.dart';
 import 'package:progetto/pages/edit_profile_page.dart';
+import 'package:progetto/pages/login_page.dart';
+import 'package:progetto/pages/main_screens.dart';
 
+import 'app_logic/auth.dart';
+import 'app_logic/storage.dart';
+import 'app_logic/database.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +26,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return CustomApp(
+      onLogged: const MainScreens(),
+      onNotLogged: const LoginPage(),
+      auth: Auth(),
+      storage: Storage(),
+      database: Database(),
+    );
+  }
+}
+
+class CustomApp extends StatelessWidget {
+  final Widget onLogged;
+  final Widget onNotLogged;
+  final Auth auth;
+  final Storage storage;
+  final Database database;
+
+  const CustomApp({
+    super.key,
+    required this.onLogged,
+    required this.onNotLogged,
+    required this.auth,
+    required this.storage,
+    required this.database,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
@@ -29,14 +61,21 @@ class MyApp extends StatelessWidget {
         EditProfilePage.routeName: (context) => const EditProfilePage(),
       },
       theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
-        fontFamily: "Montserrat",
-        snackBarTheme: const SnackBarThemeData(
-          behavior: SnackBarBehavior.floating,
-        )
-      ),
-      home: const WidgetTree(),
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+          fontFamily: "Montserrat",
+          snackBarTheme: const SnackBarThemeData(
+            behavior: SnackBarBehavior.floating,
+          )),
+      home: StreamBuilder(
+          stream: auth.authStateChanges,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return onLogged;
+            } else {
+              return onNotLogged;
+            }
+          }),
     );
   }
 }
